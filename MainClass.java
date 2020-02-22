31254570f3eac732cb8c1678f23eddee36ec8a7b
ThinkPad T61 <horriblehurry@outlook.com>
2020-02-22 02:03:11 -0600

    reformatted processes_resources.txt to be more matrix like, started writing MainClass

---
+++ MainClass.java
@@ -0,0 +1,16 @@
+/**
+ * Main class governing deadlock prevention algorithm operation
+ * @author Nirmal Panta
+ * @version 2020-02-22
+ */
+
+import java.util.Scanner;
+
+public class MainClass
+{
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+
+    }
+
+}
